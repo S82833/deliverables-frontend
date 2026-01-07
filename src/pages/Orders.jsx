b09fd0d03992f91e909d965e72554e089a44b0bb
S82833 <simonharth61@gmail.com>
2026-01-06 23:37:48 -0500

    Testing backends

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -58,7 +58,7 @@ export default function Orders() {
     const slideRefs = useRef([]);
 
     useEffect(() => {
-        fetch(`http://127.0.0.1:8000?phone=${phone || ""}`)
+        fetch(`http://127.0.0.1:8000/deliverables?phone=${phone || ""}`)
             .then(r => r.json())
             .then(data => {
                 const g = {};
